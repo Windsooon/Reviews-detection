c4163cfaeb01d91003e196797a287eb36e3bc3b9
Windsooon <wiwindson@outlook.com>
2019-07-27 21:48:22 +0800

    add stop words

--- base.py
+++ base.py
@@ -2,12 +2,13 @@ import os
 import csv
 import logging
 import numpy as np
+import pkuseg
 
 from sklearn.feature_extraction.text import CountVectorizer
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 DATA_DIR = os.path.join(BASE_DIR, 'cleaned_data')
+STOP_WORDS = ['如果', '但是', '他们', '你们', '并且', '或者', '不只', '而且', '还有', '即使', '接着', '紧接着', '尽管', '例如', '虽然', '同时', '无论', '要么', '以及', '以至', '以至于', '因此', '因为', '由于', '怎样', '这么', '这些', '那些', '只是', '只要', '如此', '对于', '只有', '比如', '不但', '不是', '不能', '不要', '不过', '不再', '差不多', '除此', '除了', '大概', '大约', '何止', '即将', '尽量', '经常', '竟然', '居然', '立刻', '马上', '难道', '偶而', '偶尔', '仍然', '我们', '正在', '一款', '可以', '这里', '找到', '非常', '一个', '按照', '最后', '所以', '以前', '优势', '左右', '这个', '还是', '这次', '相当', '有个', '离开', '不错', '之后', '一样', '一起', '真的', '有点', '敲门', '原本', '于是', '以后', '所有', '甚至', '根本', '哪怕', '中国', '美国', '德国', '法国', '外国', '中华人民共和国', '台湾', '香港', '澳门', '日本', '韩国', '西班牙', '意大利']
 
 def write_csv(csv_file, rows):
     '''
@@ -52,3 +53,22 @@ def jaccard_similarity(s1, s2):
     numerator = np.sum(np.min(vectors, axis=0))
     denominator = np.sum(np.max(vectors, axis=0))
     return numerator / denominator
+
+def accuracy_score(truth, pred):
+    '''
+    Returns accuracy score for input truth and predictions.
+    input: truth: expected value
+           pred: prediction value
+    '''
+    # Ensure that the number of predictions matches number of outcomes
+    if len(truth) == len(pred):
+        # Calculate and return the accuracy as a percent
+        return ("Predictions have an accuracy of {:.2f}%.".
+            format((truth == pred).mean()*100))
+    else:
+        return "Number of predictions does not match number of outcomes!"
+
+def cut_words(sentence):
+    seg = pkuseg.pkuseg()
+    text = seg.cut(sentence)
+    return text
